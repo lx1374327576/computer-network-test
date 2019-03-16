ed9a2907f0ce7df4400b29840d663daefcf71653
apple <apple@appledeMacBook-Pro.local>
2019-03-16 16:40:58 +0800

    add tcp example

---
+++ TCPServer.py
@@ -0,0 +1,13 @@
+from socket import *
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print('The server is ready to receive')
+while True:
+    connectionSocket, address = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    modifiedSentence = sentence.upper()
+    connectionSocket.send(modifiedSentence.encode())
+    connectionSocket.close()
